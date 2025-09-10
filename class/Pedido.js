efab771552e569d365a5fc3409867ffd7574c69f
✧ Sophia Cordeiro✧ <emilisophiassss@gmail.com>
2025-09-09 22:04:37 -0300

    Update Pedido.js

--- class/Pedido.js
+++ class/Pedido.js
@@ -1,11 +1,12 @@
 class Pedido {
-    constructor(cliente,produtos) {
-        this.cliente = cliente
-        this.carrinho = produtos
-    }
-    calcularTotal() {
-        return this.carrinho.reduce((total,produto) => total + produto.preco,0)
-}
+  constructor(cliente, produtos) {
+    this.cliente = cliente
+    this.produtos = produtos
+  }
+
+  calcularTotal() {
+    return this.produtos.reduce((total, produto) => total + produto.preco, 0)
+  }
 }
 
 module.exports = Pedido
