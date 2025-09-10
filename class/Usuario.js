1fdc2e95a73b1f727a5cc04ee02bc3136d4d364e
✧ Sophia Cordeiro✧ <emilisophiassss@gmail.com>
2025-09-09 22:02:02 -0300

    Update Usuario.js

--- class/Usuario.js
+++ class/Usuario.js
@@ -1,23 +1,23 @@
 class Usuario {
-    #nome
-    #idade
-    constructor(nome, idade){
+  #nome
+  #idade
 
-        this.#nome = nome
-        this.#idade = idade
-    }
-    acessoPainel(){
-        console.log("Acesso genérico ao painel ao sistema")
-    }
+  constructor(nome, idade) {
+    this.#nome = nome
+    this.#idade = idade
+  }
 
-    get nome(){
-        return this.#nome
-    }
+  get nome() {
+    return this.#nome
+  }
 
-    get idade(){
-        return this.#idade
-    }
+  get idade() {
+    return this.#idade
+  }
+
+  acessoPainel() {
+    return "Acesso genérico ao sistema."
+  }
 }
 
 module.exports = Usuario
-
